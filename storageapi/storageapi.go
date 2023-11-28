c6afbfc4e85fceb3acf32849dd04b30d3ac1675b
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2023-11-28 12:34:37 +0700

    feat(storageapi): delete file

--- storageapi/storageapi.go
+++ storageapi/storageapi.go
@@ -59,6 +59,17 @@ func (u GetResponse) IsOk() bool {
 	return u.Status == statusOk
 }
 
+type DeleteResponse struct {
+	FileID  string         `json:"file_id"`
+	Info    FileInfo       `json:"info"`
+	Message string         `json:"message"`
+	Status  responseStatus `json:"status"`
+}
+
+func (u DeleteResponse) IsOk() bool {
+	return u.Status == statusOk
+}
+
 type Config struct {
 	// ClientID is the application's ID.
 	ClientID string
@@ -195,6 +206,39 @@ func (s *StorageApi) Get(ctx context.Context, fileId string) (GetResponse, error
 	}
 
 	return getFileByIdResponse, nil
+}
+
+func (s *StorageApi) Delete(ctx context.Context, fileId string) (DeleteResponse, error) {
+	client := &http.Client{}
+	url := fmt.Sprintf("%s/d/files/%s", s.storageApiUrl, fileId)
+	req, err := http.NewRequestWithContext(ctx, "DELETE", url, nil)
+	if err != nil {
+		return DeleteResponse{}, fmt.Errorf("failed to create request: %w", err)
+	}
+	if err := s.setAuthorizationHeader(ctx, req); err != nil {
+		return DeleteResponse{}, fmt.Errorf("failed to set authorization header: %w", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return DeleteResponse{}, fmt.Errorf("failed to do request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return DeleteResponse{}, fmt.Errorf("failed to read response body: %w", err)
+		}
+		return DeleteResponse{}, fmt.Errorf("failed to delete file: %s", string(body))
+	}
+
+	var deleteResponse DeleteResponse
+	if err := json.NewDecoder(resp.Body).Decode(&deleteResponse); err != nil {
+		return DeleteResponse{}, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return deleteResponse, nil
 }
 
 func (s *StorageApi) detectMimeType(file multipart.File) (string, error) {
